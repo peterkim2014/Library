e0fec1b0743b6447ef2631709c4dfb42612f4bfe
Vivian Berk <vanceberk@live.com>
2023-07-22 18:19:00 -0700

    Working it out some more

--- flask_app/models/library_api.py
+++ flask_app/models/library_api.py
@@ -1,38 +1,33 @@
 import requests
+import json
 
 api_url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=R9RSHuxrGmLLXRwgPCV11WGa1huyK6iA/"
+
+def jprint(obj): 
+    text = json.dumps(obj, indent=4)
+    print(text)
 
 class Book: 
 
     #GET ALL/DEFAULT 
     def get_all(cls):
         response = requests.get(api_url)
+
+        page_results = response.json()['results']
+
         home_books = []
-        for books in response:
+
+        for books in page_results:
             book_data = {
-                'id': response['isbn10'],
-                'title': response['title']
+                'title': books['title'],
+                'author': books['author'],
+                'description': books['description'],
+                'publisher': books['publisher']
             }
             home_books.append(book_data)
         print(home_books)
-
-
-author = "Dana Simpson"
-
-response = requests.get(f"https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=R9RSHuxrGmLLXRwgPCV11WGa1huyK6iA&author=" + author)
-
-results = response.json()['results']
-
-all_titles = []
-
-for books in results: 
-    results = books['title']
-    all_titles.append(results)
-print("Book Titles")
-print(all_titles)
-
-# print(response)
-# data = response.json()
-
-# print(type(response.json()))
-# print(response.json())
+        return home_books
+    
+    #SEARCH BY TITLE
+    
+    #SEARCH BY AUTHOR
