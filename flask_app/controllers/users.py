55c0cd833e3c420b5c7d614d250fcb866dc82bb9
Vivian Berk <vanceberk@live.com>
2023-07-25 11:45:37 -0700

    Working on user registration and validation

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,49 @@
+from flask_app import app
+from flask import Flask, redirect, render_template, request, flash, session
+from ..models.user import User
+from ..models.book import Book
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+dataFormat = "%#m/%#d/%Y %I: %M %p"
+
+@app.route('/')
+def login_and_reg():
+    return render_template("welcome_page.html")
+
+@app.route('/home')
+def home_page():
+    if 'user_id' not in session: 
+        return redirect ('/')
+    else: 
+        data ={
+            'id': session['user_id']
+        }
+    data = {
+        'id': session['user_id']
+    }
+    return render_template('home.html', user=User.get_by_id(data))
+
+@app.route('/users')
+def all_users():
+    if 'user_id' not in session: 
+        return redirect ('/')
+    else: 
+        data ={
+            'id': session['user_id']
+        }
+    return render_template('users.html', users=User.get_all(), user=User.get_by_id(data))
+
+#ACTIONS/POSTS 
+@app.route('/register', methods=['POST'])
+def reg():
+    if not User.validate_reg(request.form):
+        return redirect('/')
+    data = {
+        "username": request.form['username'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.register_user(data)
+    session['user_id'] = id
+
+    return redirect('/home')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -16,41 +16,79 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-        @classmethod 
-        def get_all(cls):
-            query = "SELECT * FROM users;"
-            users = []
-            results = connectToMySQL(cls.db).query_db(query)
-            for row in results: 
-                users.append(cls(row))
-            return users
+    @classmethod 
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        users = []
+        results = connectToMySQL(cls.db).query_db(query)
+        for row in results: 
+            users.append(cls(row))
+        return users
         
-        @classmethod 
-        def get_by_id(cls,data):
-            query = """
-                    SELECT * FROM users
-                    LEFT JOIN saves ON users.id = saves.user_id
-                    LEFT JOIN books ON books.id = saves.book_id
-                    WHERE users.id = %(id)s
-                    ORDER BY saves.created_at DESC;
-                    """
-            results = connectToMySQL(cls.db).query_db(query, data)
-            user = cls(results[0])
-            for row in results: 
-                if row ['books.id'] == None: 
-                    break
-                data = {
-                    "id": row['books.id'],
-                    "title": row['title'],
-                    'author': row['author'],
-                    'pages': row['pages'],
-                    'publisher': row['publisher'],
-                    'created_at': row['created_at'],
-                    'updated_at': row['updated_at'],
+    @classmethod 
+    def get_by_id(cls,data):
+        query = """
+                SELECT * FROM users
+                LEFT JOIN saves ON users.id = saves.user_id
+                LEFT JOIN books ON books.id = saves.book_id
+                WHERE users.id = %(id)s
+                ORDER BY saves.created_at DESC;
+                """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        user = cls(results[0])
+        for row in results: 
+            if row ['books.id'] == None: 
+                break
+            data = {
+                "id": row['books.id'],
+                "title": row['title'],
+                'author': row['author'],
+                'pages': row['pages'],
+                'publisher': row['publisher'],
+                'created_at': row['created_at'],
+                'updated_at': row['updated_at'],
                 }
 
-        #POST METHODS
+#POST METHODS
+    #REGISTER USER
+    @classmethod
+    def register_user(cls,data):
+        query = """
+                INSERT INTO users(username, email, password)
+                VALUES (%(username)s, %(email)s, %(password)s);
+                """
+        return connectToMySQL(cls.db).query_db(query,data)
 
 
+    #VALIDATIONS
+    @staticmethod
+    def validate_reg(user):
+        is_valid = True
+        query = """
+                SELECT * FROM users 
+                WHERE email = %(email)s OR username = %(user_name)s;
+                """
+        results = connectToMySQL(User.db).query_db(query, user)
 
-        #VALIDATIONS
+        if len(user['user_name']) < 3: 
+            flash("User name must be longer than 3 characters", 'reg_error')
+            is_valid = False
+
+        if len(results) >= 1:
+            flash("Email address or username is already registered", 'reg_error')
+            is_valid = False
+
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address", 'reg_error')
+
+        if len(user['password']) < 8:
+            flash("Password must contain more than 8 characters", 'reg_error')
+            is_valid = False
+            
+        if user['password'] != user['confirm_password']:
+            flash("Passwords don't match", 'reg_error')
+
+        print(user['password'])
+        print(is_valid)
+
+        return is_valid 
